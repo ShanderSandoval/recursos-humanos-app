b625047f53a5bb8b506a5cf5ae8592a1f5281e86
ShanderSandoval <135709230+ShanderSandoval@users.noreply.github.com>
2024-07-10 09:07:31 -0500

    Fix errors

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
+
+function App() {
+  return (
+    <div className="container">
+     <BrowserRouter>
+      <Navegacion/>
+      <Routes>
+        <Route exact path='/' element={<ListadoEmpleados/>}/>
+        <Route exact path='/agregar' element={<AgregarEmpleado/>}/>
+      </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
